edd1cef98ed401fc4a81d0d550857cbcc6216610
dengxl <dengxl@dengxl-PC>
2016-04-03 14:37:52 +0800

    first commit

---
+++ ownjar/src/main/java/com/ownjar/App.java
@@ -0,0 +1,13 @@
+package com.ownjar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
